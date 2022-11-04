a529391a03090b0296872bb5f5ed3048132269ea
deedough <nicholasomarpadilla@ca.rr.com>
2022-11-04 11:42:44 -0700

    Added Tile Stuff

--- RubysAdventurePadilla/Assets/Scripts/RubyController.cs
+++ RubysAdventurePadilla/Assets/Scripts/RubyController.cs
@@ -11,11 +11,18 @@ public class RubyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //create 2 variables to use the unity built in axes
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
+
+        //create my movement vector variable
         Vector2 position = transform.position;
+
+        //create the movement of my character
         position.x = position.x + 3.0f * horizontal * Time.deltaTime;
         position.y = position.y + 3.0f * vertical * Time.deltaTime;
+
+        //set the new position
         transform.position = position;
     }
 }
