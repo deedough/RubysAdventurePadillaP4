77fef60219bfebf89046835f789bf4557c3d4403
deedough <nicholasomarpadilla@ca.rr.com>
2022-11-30 11:44:11 -0800

    Added Tiles and Gravity to Ruby

--- RubysAdventurePadilla/Assets/Scripts/RubyController.cs
+++ RubysAdventurePadilla/Assets/Scripts/RubyController.cs
@@ -1,28 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 public class RubyController : MonoBehaviour
 {
+    Rigidbody2D rigidbody2d;
+    float horizontal;
+    float vertical;
+
     // Start is called before the first frame update
     void Start()
     {
+        rigidbody2d = GetComponent<Rigidbody2D>();
     }
+
     // Update is called once per frame
     void Update()
     {
-        //create 2 variables to use the unity built in axes
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    }
 
-        //create my movement vector variable
-        Vector2 position = transform.position;
-
-        //create the movement of my character
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
         position.x = position.x + 3.0f * horizontal * Time.deltaTime;
         position.y = position.y + 3.0f * vertical * Time.deltaTime;
 
-        //set the new position
-        transform.position = position;
+        rigidbody2d.MovePosition(position);
     }
 }
